f1c10b581061ac550112d8a9f8f92f5791b818ce
Art Steinmetz <apsteinmetz@yahoo.com>
2019-02-13 16:29:16 -0500

    first plot done

--- unpack_dem.R
+++ unpack_dem.R
@@ -1,6 +1,82 @@
 library(raster)
-devtools::install_github("tylermorganwall/rayshader")
+#devtools::install_github("tylermorganwall/rayshader")
 library(rayshader)
 library(tidyverse)
 library(rgdal)
 library(sf)
+library(leaflet)
+source("utilities_ray.r")
+
+data = raster::raster("data/sawatch_north.img")
+sawatch_north <- matrix(raster::extract(data,raster::extent(data),buffer=10000),
+                        nrow=ncol(data),ncol=nrow(data))
+latlon_alder<-list(lat=38.374304,long=-106.1165398)
+latlon_salida<-list(lat=38.5410578,long=-105.9922354)
+lat_width <- 0.25
+lon_width <- 0.25
+
+bbox <- list(
+  p1 = list(long=latlon_alder$long-lon_width, lat = latlon_alder$lat+lat_width),
+  p2 = list(long=latlon_alder$long+lon_width, lat = latlon_alder$lat-lat_width)
+)
+
+# view bounding box.
+leaflet() %>%
+  addTiles() %>% 
+  addRectangles(
+    lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
+    lng2 = bbox$p2$long, lat2 = bbox$p2$lat,
+    fillColor = "transparent"
+  ) %>%
+  fitBounds(
+    lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
+    lng2 = bbox$p2$long, lat2 = bbox$p2$lat
+  )
+
+
+image_size <- define_image_size(bbox, major_dim = 600)
+
+elev_file <- file.path("data", "salida.tif")
+get_usgs_elevation_data(bbox, size = image_size$size, file = elev_file,
+                        sr_bbox = 4326, sr_image = 4326)
+
+# load elevation data
+elev_img <- raster::raster(elev_file)
+elev_matrix <- matrix(
+  raster::extract(elev_img, raster::extent(elev_img), buffer = 1000), 
+  nrow = ncol(elev_img), ncol = nrow(elev_img)
+)
+
+# calculate rayshader layers
+ambmat <- ambient_shade(elev_matrix, zscale = 30)
+raymat <- ray_shade(elev_matrix, zscale = 30, lambert = TRUE)
+watermap <- detect_water(elev_matrix)
+
+# plot 2D# fetch overlay image
+overlay_file <- "img/salida_map.png"
+get_arcgis_map_image(bbox, map_type = "World_Topo_Map", file = overlay_file,
+                     width = image_size$width, height = image_size$height, 
+                     sr_bbox = 4326)
+overlay_img <- png::readPNG(overlay_file)
+
+elev_matrix %>%
+  sphere_shade(texture = "imhof4") %>%
+  add_water(watermap, color = "imhof4") %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  add_overlay(overlay_img, alphalayer = 0.5) %>%
+  plot_map()
+
+zscale <- 50
+rgl::clear3d()
+elev_matrix %>% 
+  sphere_shade(texture = "imhof4") %>% 
+  add_water(watermap, color = "imhof4") %>%
+  add_overlay(overlay_img, alphalayer = 0.5) %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  plot_3d(elev_matrix, zscale = zscale, windowsize = c(1200, 1000),
+          water = FALSE, soliddepth = -max(elev_matrix)/zscale, wateralpha = 0,
+          theta = 25, phi = 30, zoom = 0.5, fov = 60)
+render_snapshot()
+
---
+++ utilities_ray.R
@@ -0,0 +1,179 @@
+#rayshader utilities from Will Bishop @wcmbiship
+
+#' Define image size variables from the given bounding box coordinates.
+#'
+#' @param bbox bounding box coordinates (list of 2 points with long/lat values)
+#' @param major_dim major image dimension, in pixels. 
+#'                  Default is 400 (meaning larger dimension will be 400 pixels)
+#'
+#' @return list with items "width", "height", and "size" (string of format "<width>,<height>")
+#'
+#' @examples
+#' bbox <- list(
+#'   p1 = list(long = -122.522, lat = 37.707),
+#'   p2 = list(long = -122.354, lat = 37.84)
+#' )
+#' image_size <- define_image_size(bbox, 600)
+#' 
+define_image_size <- function(bbox, major_dim = 400) {
+  # calculate aspect ration (width/height) from lat/long bounding box
+  aspect_ratio <- abs((bbox$p1$long - bbox$p2$long) / (bbox$p1$lat - bbox$p2$lat))
+  # define dimensions
+  img_width <- ifelse(aspect_ratio > 1, major_dim, major_dim*aspect_ratio) %>% round()
+  img_height <- ifelse(aspect_ratio < 1, major_dim, major_dim/aspect_ratio) %>% round()
+  size_str <- paste(img_width, img_height, sep = ",")
+  list(height = img_height, width = img_width, size = size_str)
+}
+
+
+
+
+#' Download USGS elevation data from the ArcGIS REST API.
+#'
+#' @param bbox bounding box coordinates (list of 2 points with long/lat values)
+#' @param size image size as a string with format "<width>,<height>"
+#' @param file file path to save to. Default is NULL, which will create a temp file.
+#' @param sr_bbox Spatial Reference code for bounding box
+#' @param sr_image Spatial Reference code for elevation image
+#' 
+#' @details This function uses the ArcGIS REST API, specifically the 
+#' exportImage task. You can find links below to a web UI for this
+#' rest endpoint and API documentation.
+#' 
+#' Web UI: https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer/exportImage
+#' API docs: https://developers.arcgis.com/rest/services-reference/export-image.htm
+#'
+#' @return file path for downloaded elevation .tif file. This can be read with
+#' \code{read_elevation_file()}.
+#'
+#' @examples
+#' bbox <- list(
+#'   p1 = list(long = -122.522, lat = 37.707),
+#'   p2 = list(long = -122.354, lat = 37.84)
+#' )
+#' image_size <- define_image_size(bbox, 600)
+#' elev_file <- get_usgs_elevation_data(bbox, size = image_size$size)
+#' 
+get_usgs_elevation_data <- function(bbox, size = "400,400", file = NULL, 
+                                    sr_bbox = 4326, sr_image = 4326) {
+  require(httr)
+  
+  # TODO - validate inputs
+  
+  url <- parse_url("https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer/exportImage")
+  res <- GET(
+    url, 
+    query = list(
+      bbox = paste(bbox$p1$long, bbox$p1$lat, bbox$p2$long, bbox$p2$lat,
+                   sep = ","),
+      bboxSR = sr_bbox,
+      imageSR = sr_image,
+      size = size,
+      format = "tiff",
+      pixelType = "F32",
+      noDataInterpretation = "esriNoDataMatchAny",
+      interpolation = "+RSP_BilinearInterpolation",
+      f = "json"
+    )
+  )
+  
+  if (status_code(res) == 200) {
+    body <- content(res, type = "application/json")
+    # TODO - check that bbox values are correct
+    # message(jsonlite::toJSON(body, auto_unbox = TRUE, pretty = TRUE))
+    
+    img_res <- GET(body$href)
+    img_bin <- content(img_res, "raw")
+    if (is.null(file)) 
+      file <- tempfile("elev_matrix", fileext = ".tif")
+    writeBin(img_bin, file)
+    message(paste("image saved to file:", file))
+  } else {
+    warning(res)
+  }
+  invisible(file)
+}
+
+
+
+#' Download a map image from the ArcGIS REST API
+#'
+#' @param bbox bounding box coordinates (list of 2 points with long/lat values)
+#' @param map_type map type to download - options are World_Street_Map, World_Imagery, World_Topo_Map
+#' @param file file path to save to. Default is NULL, which will create a temp file.
+#' @param width image width (pixels)
+#' @param height image height (pixels)
+#' @param sr_bbox Spatial Reference code for bounding box
+#' 
+#' @details This function uses the ArcGIS REST API, specifically the 
+#' "Execute Web Map Task" task. You can find links below to a web UI for this
+#' rest endpoint and API documentation.
+#' 
+#' Web UI: https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task/execute
+#' API docs: https://developers.arcgis.com/rest/services-reference/export-web-map-task.htm
+#'
+#' @return file path for the downloaded .png map image
+#'
+#' @examples
+#' bbox <- list(
+#'   p1 = list(long = -122.522, lat = 37.707),
+#'   p2 = list(long = -122.354, lat = 37.84)
+#' )
+#' image_size <- define_image_size(bbox, 600)
+#' overlay_file <- get_arcgis_map_image(bbox, width = image_size$width,
+#'                                      height = image_size$height)
+#' 
+get_arcgis_map_image <- function(bbox, map_type = "World_Street_Map", file = NULL, 
+                                 width = 400, height = 400, sr_bbox = 4326) {
+  require(httr)
+  require(glue) 
+  require(jsonlite)
+  
+  url <- parse_url("https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task/execute")
+  
+  # define JSON query parameter
+  web_map_param <- list(
+    baseMap = list(
+      baseMapLayers = list(
+        list(url = jsonlite::unbox(glue("https://services.arcgisonline.com/ArcGIS/rest/services/{map_type}/MapServer",
+                                        map_type = map_type)))
+      )
+    ),
+    exportOptions = list(
+      outputSize = c(width, height)
+    ),
+    mapOptions = list(
+      extent = list(
+        spatialReference = list(wkid = jsonlite::unbox(sr_bbox)),
+        xmax = jsonlite::unbox(max(bbox$p1$long, bbox$p2$long)),
+        xmin = jsonlite::unbox(min(bbox$p1$long, bbox$p2$long)),
+        ymax = jsonlite::unbox(max(bbox$p1$lat, bbox$p2$lat)),
+        ymin = jsonlite::unbox(min(bbox$p1$lat, bbox$p2$lat))
+      )
+    )
+  )
+  
+  res <- GET(
+    url, 
+    query = list(
+      f = "json",
+      Format = "PNG32",
+      Layout_Template = "MAP_ONLY",
+      Web_Map_as_JSON = jsonlite::toJSON(web_map_param))
+  )
+  
+  if (status_code(res) == 200) {
+    body <- content(res, type = "application/json")
+    message(jsonlite::toJSON(body, auto_unbox = TRUE, pretty = TRUE))
+    if (is.null(file)) 
+      file <- tempfile("overlay_img", fileext = ".png")
+    
+    img_res <- GET(body$results[[1]]$value$url)
+    img_bin <- content(img_res, "raw")
+    writeBin(img_bin, file)
+    message(paste("image saved to file:", file))
+  } else {
+    message(res)
+  }
+  invisible(file)
+}
