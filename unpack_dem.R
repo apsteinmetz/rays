fbfb1297d6ec7e2345862bb02868b9ebdd1bb826
apsteinmetz <apsteinmetz@yahoo.com>
2019-03-06 16:46:26 -0500

    good render of Salida, CO

--- unpack_dem.R
+++ unpack_dem.R
@@ -8,13 +8,13 @@ library(leaflet)
 source("utilities_ray.r")
 
 latlon_alder<-list(lat=38.374304,long=-106.1165398)
-latlon_salida<-list(lat=38.5410578,long=-105.9922354)
-lat_width <- 0.25
-lon_width <- 0.25
+latlon_salida<-list(lat=38.53,long=-105.9922354)
+lat_width <- 0.03
+lon_width <- 0.05
 
 bbox <- list(
-  p1 = list(long=latlon_alder$long-lon_width, lat = latlon_alder$lat+lat_width),
-  p2 = list(long=latlon_alder$long+lon_width, lat = latlon_alder$lat-lat_width)
+  p1 = list(long=latlon_salida$long-lon_width, lat = latlon_salida$lat+lat_width),
+  p2 = list(long=latlon_salida$long+lon_width, lat = latlon_salida$lat-lat_width)
 )
 
 # view bounding box.
@@ -31,7 +31,7 @@ leaflet() %>%
   )
 
 
-image_size <- define_image_size(bbox, major_dim = 600)
+image_size <- define_image_size(bbox, major_dim = 1200)
 
 elev_file <- file.path("data", "salida.tif")
 get_usgs_elevation_data(bbox, size = image_size$size, file = elev_file,
@@ -51,29 +51,39 @@ raymat <- ray_shade(elev_matrix, zscale = 30, lambert = TRUE)
 
 # plot 2D# fetch overlay image
 overlay_file <- "img/salida_map.png"
-get_arcgis_map_image(bbox, map_type = "World_Topo_Map", file = overlay_file,
+get_arcgis_map_image(bbox, map_type = "World_Imagery", file = overlay_file,
                      width = image_size$width, height = image_size$height, 
                      sr_bbox = 4326)
 overlay_img <- png::readPNG(overlay_file)
 
+# plot 2D# fetch overlay image
+overlay_file2 <- "img/salida_map2.png"
+get_arcgis_map_image(bbox, map_type = "World_Street_Map", file = overlay_file2,
+                     width = image_size$width, height = image_size$height, 
+                     sr_bbox = 4326)
+overlay_img2 <- png::readPNG(overlay_file2)
+
+
 elev_matrix %>%
-  sphere_shade(texture = "imhof4") %>%
+  sphere_shade(texture = "imhof1") %>%
   # add_water(watermap, color = "imhof4") %>%
   add_shadow(raymat, max_darken = 0.5) %>%
   add_shadow(ambmat, max_darken = 0.5) %>%
-  add_overlay(overlay_img, alphalayer = 0.5) %>%
+  add_overlay(overlay_img, alphalayer = 0.9) %>%
+  add_overlay(overlay_img2, alphalayer = 0.2) %>%
   plot_map()
 
-zscale <- 50
+zscale <- 10
 rgl::clear3d()
 elev_matrix %>% 
-  sphere_shade(texture = "imhof4") %>% 
+  sphere_shade(texture = "imhof1") %>% 
   # add_water(watermap, color = "imhof4") %>%
-  add_overlay(overlay_img, alphalayer = 0.5) %>%
+  add_overlay(overlay_img, alphalayer = 0.9) %>%
+  add_overlay(overlay_img2, alphalayer = 0.4) %>%
   add_shadow(raymat, max_darken = 0.5) %>%
   add_shadow(ambmat, max_darken = 0.5) %>%
   plot_3d(elev_matrix, zscale = zscale, windowsize = c(1200, 1000),
-          water = FALSE, soliddepth = -max(elev_matrix)/zscale, wateralpha = 0,
+          water = FALSE, soliddepth = "auto", wateralpha = 0,
           theta = 25, phi = 30, zoom = 0.5, fov = 60)
 render_snapshot()
 
