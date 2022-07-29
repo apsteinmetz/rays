21d579ed512f9972277ac4bc931f9b581d2c5660
Art Steinmetz <apsteinmetz@yahoo.com>
2022-07-29 17:51:50 -0400

    render poly test

--- test.R
+++ test.R
@@ -1,30 +1,48 @@
-# test raster plotting
 library(raster)
 library(tidyverse)
-
-m1 <- matrix(1,ncol=100,nrow=120)
-
-r1 <- raster::raster(m1)
-
-full_extent <- extent(c(1,100,1,120))
-extent(r1) <- orig_extent
-# looks as we expect, all 1
-plot(r1)
-
-map_only_extent <- extent(1,100,1,110)
-extent(r1)
-
-r2 <- raster::crop(r1,map_only_extent) %>% 
-  raster::extend(full_extent,value=0)
-
-r2
-m2 <- as.matrix(r2)
+library(rayshader)
+library(rayrender)
 
 
+if(interactive()) {
 
-# set first, most southerly row, to zero
-m1[1,] = 0
+rgl::clear3d()
+#Render the county borders as polygons in Monterey Bay
+montereybay %>%
+  sphere_shade(texture = "desert") %>%
+  add_shadow(ray_shade(montereybay,zscale=50)) %>%
+  plot_3d(montereybay,water=TRUE, windowsize=800, watercolor="dodgerblue")
+render_camera(theta=140,  phi=55, zoom = 0.85, fov=30)
 
-plot(r2)
-# top, most northerly row is displayed as zero
+#We will apply a negative buffer to create space between adjacent polygons:
+mont_county_buff = sf::st_simplify(sf::st_buffer(monterey_counties_sf,-0.003), dTolerance=0.001)
+mont_county_buff = sf::st_simplify(monterey_counties_sf, dTolerance=0.1)
 
+render_polygons(mont_county_buff, 
+                extent = attr(montereybay,"extent"), top=10,
+                parallel=TRUE)
+render_snapshot()
+
+#We can specify the bottom of the polygons as well. Here I float the polygons above the surface
+#by specifying the bottom argument. We clear the previous polygons with `clear_previous = TRUE`.
+render_camera(theta=-60,  phi=20, zoom = 0.85, fov=0)
+render_polygons(mont_county_buff, 
+                extent = attr(montereybay,"extent"), bottom = 190, top=200,
+                parallel=TRUE,clear_previous=TRUE)
+render_snapshot()
+
+#We can set the height of the data to a column in the sf object: we'll use the land area.
+#We'll have to scale this value because it's max value is 2.6 billion:
+render_camera(theta=-60,  phi=60, zoom = 0.85, fov=30)
+render_polygons(mont_county_buff, 
+                extent = attr(montereybay,"extent"), data_column_top = "ALAND",
+                scale_data = 300/(2.6E9), color="chartreuse4",
+                parallel=TRUE,clear_previous=TRUE)
+render_snapshot()        
+
+#This function also works with `render_highquality()`
+render_highquality(samples=400, clamp_value=10)
+rgl::rgl.close()
+
+}
+[Package rayshader version 0.24.10 Index]
