51ce898143f6139351eb929a4a27e5d603efd9e0
Art Steinmetz <apsteinmetz@yahoo.com>
2023-08-29 16:37:45 -0400

    get path from osmr

--- make_path_gwl.R
+++ make_path_gwl.R
@@ -6,125 +6,78 @@ library(raster)
 library(elevatr)
 library(png)
 library(sf)
+library(terra)
+library(osrm)
 
 
+start_point <- c(-74.2516694,41.0288009) # where skyline drive meets i-287
+end_point <- c(-74.28869990,41.22772724) # north end of windermere ave. in Greenwood Lake
+map_ras <- raster::stack("img/gwl_trip.tif")
 
 # Use this as our common extent#
-full_extent <- extent(water_bodies)
+full_extent <- extent(map_ras)
 
 # get water
 # https://hub.arcgis.com/datasets/esri::usa-detailed-water-bodies/explore
+# water_bodies = st_read("data/gwl/water/USA_Detailed_Water_Bodies.shp")
 
-water_bodies = st_read("data/gwl/water/USA_Detailed_Water_Bodies.shp")
+gwl_route_detailed <- osrm::osrmRoute(start_point,end_point,overview = "full")
+gwl_route_simple <- osrm::osrmRoute(start_point,end_point,overview = "simplified")
+plot(st_geometry(gwl_route_detailed), col = "red", lwd = 2)
 
-# get roads
-nj_passaic_roads = st_read("data/usa_roads/nj/tl_2022_34031_roads.shp") 
-nj_bergen_roads = st_read("data/usa_roads/nj/tl_2022_34003_roads.shp") 
-ny_orange_roads = st_read("data/usa_roads/ny/tl_2022_36071_roads.shp")
-
-seg_1 <- nj_bergen_roads |> filter(FULLNAME %in% c("Skyline Dr")) |> st_union()
-seg_2 <- nj_passaic_roads |> filter(FULLNAME %in% c("Greenwood Lake Tpke","Lakeside Rd","Skyline Dr"))  |> st_union()
-seg_3 <- ny_orange_roads |> filter(FULLNAME %in% c("Jersey Ave","Windermere Ave")) |> st_union()
-
-st_geometrycollection(seg_1,seg_2)
-geometry = st_sfc(lapply(c(seg_1,seg_2,seg_3), function(x) st_geometrycollection()))
-
-
-# map_ras <- raster::stack("img/Greenwood Lake-1910_modified.tif")
-# cropped_ras <- raster::stack("img/Greenwood Lake-1910_cropped.tif")
-# map_img <- readPNG("img/Greenwood Lake-1910_modified.png")
-
-# full_extent <- extent(map_img)
-#reduce size for quicker plotting
-#small_ras <- raster::aggregate(map_ras,fact=5)
-
-# change extent to visible map and crop
-
-# -------------------------------------------------------------
-# Download external elevation data
-# get elevation data
-
-raw_elevation <- get_elev_raster(raster(small_ras), z = 11)
+raw_elevation <- get_elev_raster(raster(map_ras), prj = "+proj=longlat +datum=WGS84 +no_defs",z = 12)
 elevation <- crop(raw_elevation,extent(map_ras))
-crop_elevation <- crop(raw_elevation,extent(cropped_ras))
-
-base_raster <- elevation * 0
-elevation <- merge(crop_elevation, base_raster)
-
-
 
 elev_matrix <- elevation |> 
   raster_to_matrix()
 
+route_overlay <- generate_line_overlay(gwl_route_detailed,
+                                       full_extent,
+                                       heightmap = elev_matrix,
+                                       color = "black",
+                                       linewidth = 2)
 
-# r get roads
-if (fs::file_exists('data/gwl_roads.rdata')) {
-  load('data/gwl_roads.rdata')
-} else {
-  gwl_roads <- osmdata::opq(st_bbox(cropped_ras)) %>%
-    osmdata::add_osm_feature("highway") %>%
-    osmdata::osmdata_sf()
-  save(gwl_roads, file = 'data/gwl_roads.rdata')
-}
+shadow_lamb <- lamb_shade(elev_matrix)
+shadow_ray <- ray_shade(elev_matrix,lambert=FALSE)
+shadow_ambient <- ambient_shade(elev_matrix)
+water_overlay <- detect_water(elev_matrix,min_area = length(elev_matrix)/4000)
 
-# Take just the road layer and transform coordinates to our existing projection
-gwl_lines <- sf::st_transform(gwl_roads$osm_lines,
-                                   crs = raster::crs(cropped_ras)) |> 
-   st_crop(st_bbox(extent(cropped_ras)))
+elev_matrix |> 
+  #sphere_shade() |> 
+  height_shade(texture = (grDevices::colorRampPalette(c("#6AA85B", "orange2")))(256)) |> 
+  add_water(water_overlay,color="steelblue") |> 
+  add_shadow(shadow_ray) |> 
+  add_shadow(shadow_lamb) |> 
+  add_shadow(shadow_ambient) |> 
+  add_overlay(route_overlay) |> 
+  plot_3d(elev_matrix,zscale = 8)
 
+render_path(gwl_route_simple, 
+            extent = full_extent, 
+            heightmap = elev_matrix, 
+            color="red", linewidth = 4, 
+            zscale=8, offset = 50)
 
+camera_coords = convert_path_to_animation_coords(
+  gwl_route_simple,
+  extent = full_extent, 
+  offset_lookat = 1,
+  heightmap = elev_matrix, 
+  zscale = 8,
+  offset = 50, 
+  # not needed for single geometry
+  # reorder = TRUE,
+  #reorder_merge_tolerance = 0.45, 
+  # reorder_duplicate_tolerance = 0.20,
+  resample_path_evenly = TRUE,
+  frames = 1080, 
+  follow_camera = TRUE,
+  follow_fixed = TRUE,
+  follow_fixed_offset = c(10,10,10),
+  damp_motion = TRUE, 
+  damp_magnitude = 0.5
+)
 
-
-
-# gwl_lines |> ggplot(aes()) + geom_sf(color = "orange")
-# # a rayshader object we pre-generate now for speed later
-# road_overlay <-  generate_line_overlay(
-#   gwl_lines,
-#   color = "blue",
-#   # dummy matrix to get roads at full resolution
-#   # even though actual heightmap is lower res
-#   heightmap = as.matrix(map_ras$Greenwood.Lake.1910_modified_1),
-#   extent = extent(extent(map_ras)),
-#   linewidth = 1
-# )
-
-# texture_shadow <- texture_shade(elev_matrix,detail=8/10,contrast=9,brightness = 11)
-
-zscale = 12
-
-# ambmat <- ambient_shade(elev_matrix, zscale = zscale,
-#                        multicore = TRUE)
-raymat <- ray_shade(elev_matrix, sunaltitude = 25,zscale = zscale, 
-                    lambert = TRUE,
-                    multicore = TRUE)
-
-watermap <- detect_water(elev_matrix,min_area = length(elev_matrix)/1200)
-# show one view
-elev_matrix %>%
-  sphere_shade(texture = "imhof1") |> 
-  add_water(watermap) |> 
-  # add_overlay(map_img, alphalayer = 1,rescale_original = FALSE) %>%
-  # add_overlay(road_overlay) |> 
-  # add_shadow(texture_shadow, max_darken = 0.1) %>%
-  add_shadow(raymat, max_darken = 0.3) %>%
-  # add_shadow(ambmat, max_darken = 0.5) %>%
-  plot_map()
- 
-
-#Plot in 3D
-elev_matrix %>% 
-  sphere_shade() |> 
-  add_water(watermap) |> 
-  # add_overlay(map_img, alphalayer = 1,rescale_original = FALSE) %>%
-  # add_overlay(road_overlay) |> 
-  # add_shadow(raymat, max_darken = 0.3) %>%
-  #add_shadow(ambmat, max_darken = 0.5) %>%
-  plot_3d(elev_matrix,zscale = zscale)
-
-
-rgl::close3d()
-#Render snapshot with depth of field
-render_depth(focus=0.982,focallength = 4000)
-
-
-
+render_highquality(animation_camera_coords = camera_coords[nrow(camera_coords):1,], width=800, height=800,
+                   samples=128, line_radius=0.10, sample_method = "sobol_blue", 
+                   filename="frames/gwl_")
