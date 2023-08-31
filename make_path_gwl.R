2b2a93700e5b00d7ae0bc7e40cc721e68fc3c8ba
Art Steinmetz <apsteinmetz@yahoo.com>
2023-08-31 08:29:34 -0400

    use maptiles package instead of basemaps

--- make_path_gwl.R
+++ make_path_gwl.R
@@ -8,36 +8,53 @@ library(png)
 library(sf)
 library(terra)
 library(osrm)
-devtools::install_github("16EAGLE/basemaps")
-library(basemaps)
+devtools::install_github("riatelab/maptiles")
+library(maptiles)
 
 
 start_point <- c(-74.2516694,41.0288009) # where skyline drive meets i-287
 end_point <- c(-74.28869990,41.22772724) # north end of windermere ave. in Greenwood Lake
-map_ras <- raster::stack("img/gwl_trip.tif")
+map_ras <- terra::rast("img/gwl_trip.tif") |> 
+  terra::project("EPSG:3857")
 
 # Use this as our common extent#
-full_extent <- extent(map_ras)
+full_extent <- terra::ext(map_ras)
+
+# get basemap tiles
+gwl_basemap <- maptiles::get_tiles(map_ras,
+                    provider="Thunderforest.Outdoors",
+                    crop = TRUE,
+                    zoom = 14,
+                    apikey = Sys.getenv("THUNDERFOREST_KEY"))
+
+
+terra::writeRaster(gwl_basemap,"img/gwl_basemap.png")
+gwl_png <- readPNG("img/gwl_basemap.png")
 
 # get water
 # https://hub.arcgis.com/datasets/esri::usa-detailed-water-bodies/explore
 # water_bodies = st_read("data/gwl/water/USA_Detailed_Water_Bodies.shp")
 
-gwl_route_detailed <- osrm::osrmRoute(start_point,end_point,overview = "full")
-gwl_route_simple <- osrm::osrmRoute(start_point,end_point,overview = "simplified")
+gwl_route_detailed <- osrm::osrmRoute(start_point,end_point,overview = "full") |> 
+  st_transform(crs(map_ras))
+gwl_route_simple <- osrm::osrmRoute(start_point,end_point,overview = "simplified") |> 
+  st_transform(crs(map_ras))
+
 plot(st_geometry(gwl_route_detailed), col = "red", lwd = 2)
 
-raw_elevation <- get_elev_raster(raster(map_ras), prj = "+proj=longlat +datum=WGS84 +no_defs",z = 12)
-elevation <- crop(raw_elevation,extent(map_ras))
+raw_elevation <- get_elev_raster(raster(map_ras),z = 12)
+elevation <- raw_elevation |> 
+  rast() |> 
+  terra::crop(map_ras)
 
 elev_matrix <- elevation |> 
   raster_to_matrix()
 
 route_overlay <- generate_line_overlay(gwl_route_detailed,
-                                       full_extent,
+                                       map_ras,
                                        heightmap = elev_matrix,
-                                       color = "black",
-                                       linewidth = 1)
+                                       color = "red",
+                                       linewidth = 2)
 
 shadow_lamb <- lamb_shade(elev_matrix)
 shadow_ray <- ray_shade(elev_matrix,lambert=FALSE)
@@ -45,13 +62,15 @@ shadow_ambient <- ambient_shade(elev_matrix)
 water_overlay <- detect_water(elev_matrix,min_area = length(elev_matrix)/4000)
 
 elev_matrix |> 
-  #sphere_shade() |> 
-  height_shade(texture = (grDevices::colorRampPalette(c("#6AA85B", "orange2")))(256)) |> 
-  add_water(water_overlay,color="steelblue") |> 
+  sphere_shade() |> 
+  # height_shade(texture = (grDevices::colorRampPalette(c("#6AA85B", "orange2")))(256)) |> 
+#  add_water(water_overlay,color="steelblue") |> 
+  add_overlay(gwl_png,rescale_original = TRUE) |> 
   add_shadow(shadow_ray) |> 
   add_shadow(shadow_lamb) |> 
-#  add_shadow(shadow_ambient) |> 
+  add_shadow(shadow_ambient) |> 
   add_overlay(route_overlay) |> 
+#  plot_map()
   plot_3d(elev_matrix,zscale = 7)
 
 render_path(gwl_route_detailed, 
@@ -64,7 +83,7 @@ render_path(gwl_route_detailed,
 
 camera_coords <- 
   convert_path_to_animation_coords(
-  gwl_route_detailed,
+  gwl_route_simple,
   extent = full_extent, 
   heightmap = elev_matrix, 
   zscale = 7,
@@ -75,9 +94,9 @@ camera_coords <-
   resample_path_evenly = TRUE,
   offset = 10, 
   offset_lookat = 10,
-  # altitude = 400,
-  follow_camera = FALSE,
-  # follow_distance = 5,
+  altitude = 400,
+  follow_camera = TRUE,
+  follow_distance = 100,
   follow_fixed = FALSE,
 #  follow_fixed_offset = c(10,10,10),
   follow_angle = 15,
@@ -87,13 +106,14 @@ camera_coords <-
 
 )
 
-render_camera()
+# render_camera()
 # rgl::lines3d(camera_coords[,1:3], color="black", tag = "path3d")
 
 render_highquality(samples=128, line_radius=0.5, sample_method = "sobol_blue")
 
 render_highquality(animation_camera_coords = camera_coords, width=800, height=800,
-                   samples=128, line_radius=0.5, sample_method = "sobol_blue", 
-                   light = TRUE,
+                   samples=128, line_radius=0.5, sample_method = "sobol_blue",
+                   ambient_light = TRUE,
                    filename="frames/gwl_")
-
+# external program
+# ffmpeg -framerate 5 -i gwl_%d.png -pix_fmt yuv420p roadtrip.mp4
